600185df6f5f45b27ca87106ac5e3fb4a924a407
Kirill <27140985+lerand94@users.noreply.github.com>
2022-09-13 19:22:50 +0300

    adding AllPosts cmp

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,42 @@
-const SinglePostPage = () => {};
+import AllPosts from "../../components/posts/AllPosts";
+
+const DUMMY_POSTS = [
+  {
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs2",
+    excerpt:
+      "NextJS is a the React framework for production full-stack development",
+  },
+  {
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs3",
+    excerpt:
+      "NextJS is a the React framework for production full-stack development",
+  },
+  {
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs4",
+    excerpt:
+      "NextJS is a the React framework for production full-stack development",
+  },
+  {
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    date: "2022-02-10",
+    slug: "getting-started-with-nextjs",
+    excerpt:
+      "NextJS is a the React framework for production full-stack development",
+  },
+];
+
+const SinglePostPage = () => {
+  return <AllPosts posts={DUMMY_POSTS} />;
+};
 
 export default SinglePostPage;
