f37e25747cd3b7198ff53766f04892f99740ed2c
Kirill <27140985+lerand94@users.noreply.github.com>
2022-09-13 18:26:31 +0300

    adding a PostGrid and PostItem cmps

--- components/HomePage/FeaturedPosts.js
+++ components/HomePage/FeaturedPosts.js
@@ -1,3 +1,11 @@
-const FeaturedPosts = () => {};
+import classes from "./FeaturedPosts.module.css";
+
+const FeaturedPosts = () => {
+  return (
+    <section className={classes.latest}>
+      <h2>Featured Posts</h2>
+    </section>
+  );
+};
 
 export default FeaturedPosts;
